7da75bbfed7d71e5095a8d00305d4a92ac399b2d
Jyosthna2 <srirangapuramjyosthna@gmail.com>
2023-10-18 15:19:16 +0530

    Sign in && Sign Up page Login Form

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+        <img 
+        className="w-90 h-10 p-1 m-2 "
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Logonetflix.png/800px-Logonetflix.png" alt='logo'/>
+    </div>
+  )
+}
+
+export default Header
